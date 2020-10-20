3c15d802e3dd18976e79235ffa0e51e1125b3206
baharriyan <indonesiariyan@gmail.com>
2020-10-20 18:59:14 +0700

    first commit

---
+++ PBOIF2_10116416_LATIHAN9_Teks/src/pboif2_10116416_latihan9_teks/PBOIF2_10116416_Latihan9_Teks.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10116416_latihan9_teks;
+/**
+ *
+ * @author Riyan Bahar
+ * Nama  : Riyan Bahar
+ * Kelas : IF-2
+ * Nim   : 10116416
+ * 
+ */
+public class PBOIF2_10116416_Latihan9_Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
